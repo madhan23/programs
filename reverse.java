e9e59217c0ffaee53409e11154b8f0d02696cb2f
madhan23 <madhanraju23@gmail.com>
2017-04-19 18:23:44 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int ts,m,tt=0;
+		Scanner s= new Scanner(System.in);
+		ts=s.nextInt();
+		while(ts>0)
+		{
+			tt=ts%10;
+			ts=ts/10;
+		
+		System.out.print(tt);
+}
+}
+}
