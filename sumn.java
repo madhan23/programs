2df76bce3f2abeada1cff6da312db3f23552bef3
madhan23 <madhanraju23@gmail.com>
2017-04-20 17:31:53 +0530

    Create sumn.java

---
+++ sumn.java
@@ -0,0 +1,22 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+int n,ss=0,j;
+n=s.nextInt();
+for(j=1;j<=n;j++)
+{
+    ss+=j;
+}
+System.out.println("sum:"+ss);
+	}
+}	
+	
