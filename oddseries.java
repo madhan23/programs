1861c70011b82bdd496d8730d5db0083f8e26c61
madhan23 <madhanraju23@gmail.com>
2017-04-24 07:42:12 +0530

    Create oddseries.java

---
+++ oddseries.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+class madhan {
+	public static void main (String[] args) {
+	    int i,n,v,cc=0,start,end;   
+	 Scanner c=new Scanner(System.in);
+	 start=c.nextInt();
+	 end=c.nextInt();
+     System.out.println("odd number between "+start+ " to " +end);
+      for(v=start;v<=end;v++)
+      {
+         if(v%2!=0)
+          {
+             System.out.println(v);
+      
+          }
+      }
+      
+	} 
+} 
+      
